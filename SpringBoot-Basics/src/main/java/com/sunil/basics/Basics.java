3789357926cd4ef05bc1f87c4f6b835f68430855
SunilChormale <sunilschormale01@gmail.com>
2022-05-09 17:00:20 +0530

    Basics of springBoot

---
+++ SpringBoot-Basics/src/main/java/com/sunil/basics/Basics.java
@@ -0,0 +1,13 @@
+package com.sunil.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Basics {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Basics.class,args);
+    }
+
+}
